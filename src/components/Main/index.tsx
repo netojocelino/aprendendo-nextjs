5ca893a970756ce5a568921663eb682b5a65f49c
Jocelino Neto <netojocelino@gmail.com>
2021-03-13 00:40:55 -0300

    :white_check_mark: Add 'React Avançado' landing page with tests

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,7 +1,23 @@
-const Main = ({ title = 'React com NextJS' }) => (
-  <div>
-    <h1>{title}</h1>
-  </div>
+import * as S from './styles';
+
+const Main = ({
+  title = 'React com NextJS',
+  description = 'Just another boilerplate'
+}) => (
+  <S.Wrapper>
+    <S.Logo
+      src={`/img/logo.svg`}
+      alt={`Imagem de um átomo e texto React Avançado`}
+    />
+    <S.Title>{title}</S.Title>
+
+    <S.Description>{description}</S.Description>
+
+    <S.Illustration
+      src={`/img/hero-illustration.svg`}
+      alt={`Imagem de pessoa em frente de computador com três telas com código`}
+    />
+  </S.Wrapper>
 );
 
 export default Main;
--- src/components/Main/test.tsx
+++ src/components/Main/test.tsx
@@ -12,6 +12,30 @@ describe(`<Main />`, () => {
       })
     ).toBeInTheDocument();
 
+    expect(
+      screen.getByRole('heading', {
+        name: /just another boilerplate/i
+      })
+    ).toBeInTheDocument();
+
+    expect(
+      screen.getByAltText(/Imagem de um átomo e texto React Avançado/i)
+    ).toContainHTML(`/img/logo.svg`);
+
+    expect(
+      screen.getByAltText(
+        `Imagem de pessoa em frente de computador com três telas com código`
+      )
+    ).toContainHTML(`/img/hero-illustration.svg`);
+
     expect(container.firstChild).toMatchSnapshot();
+  });
+
+  it(`should render the colors correctly`, () => {
+    const { container } = render(<Main />);
+
+    expect(container.firstChild).toHaveStyle({
+      'background-color': '#06092b'
+    });
   });
 });
